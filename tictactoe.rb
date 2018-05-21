70b5ee409fb0f5ed500ea7d33afe94775d3bc28d
oscar ortiz <ortizg.oscar@gmail.com>
2018-05-21 14:05:11 -0300

    refactord complete

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,34 +1,70 @@
+=begin
+- [checked] Adicionar tratamento de entradas válidas.
+- Permitir a seleção da modalidade de jogo: fácil, médio ou
+difícil
+(hoje está sempre no difícil).
+- Permitir a seleção do tipo de jogo: computador vs computador,
+ player vs player ou computador vs player.
+
+O objetivo do desafio então é melhorar o código e adicionar
+o maior número de features possíveis, incluindo as mencionadas
+anteriormente e quaisquer outras que você julgue necessária
+para evoluir e melhorar o jogo. Qualquer melhoria ou feature
+que julgue ser necessária será muito bem vinda.
+=end
+
 class Game
   def initialize
+    # @board = (1..9).to_a # Removed box
     @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
     @com = "X" # the computer's marker
     @hum = "O" # the user's marker
   end
 
+  def print_board
+    puts "\n\n #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]}"
+    puts "Your turn \n\n"
+  end
+
   def start_game
     # start by printing the board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
-    puts "Enter [0-8]:"
+    puts "\nChoose and empty box from [0-8]:\n\n"
+    print_board
     # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
+    until game_over?
       get_human_spot
-      if !game_is_over(@board) && !tie(@board)
-        eval_board
-      end
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+      eval_board unless (game_over?)
+      print_board
     end
+    puts 'Is a tie' if tie?
     puts "Game over"
   end
 
-  def get_human_spot
-    spot = nil
-    until spot
-      spot = gets.chomp.to_i
-      if @board[spot] != "X" && @board[spot] != "O"
-        @board[spot] = @hum
-      else
-        spot = nil
+  def user_input_valid?(user_input)
+    available_spaces.include?(user_input)
+  end
+
+  def get_valid_user_input(user_input)
+    if user_input_valid?(user_input)
+      return user_input
+    else
+      until user_input_valid?(user_input)
+        puts "'#{user_input}'Is not a valid cell option \nPlease try again"
+        user_input = gets.chomp
       end
+    return user_input
+    end
+  end
+
+  def get_human_spot
+    @board[get_valid_user_input(gets.chomp).to_i] = @hum
+  end
+
+  def draw_center
+    if available_spaces.include?(@board[4])
+      @board[4] = @com
+    else
+      return false
     end
   end
 
@@ -39,7 +75,7 @@ class Game
         spot = 4
         @board[spot] = @com
       else
-        spot = get_best_move(@board, @com)
+        spot = get_best_move(@com)
         if @board[spot] != "X" && @board[spot] != "O"
           @board[spot] = @com
         else
@@ -49,53 +85,87 @@ class Game
     end
   end
 
-  def get_best_move(board, next_player, depth = 0, best_score = {})
-    available_spaces = []
-    best_move = nil
-    board.each do |s|
+  def available_spaces
+    spaces = []
+    @board.each do |s|
       if s != "X" && s != "O"
-        available_spaces << s
+        spaces << s
       end
     end
+    return spaces
+  end
+
+  def find_winning_move
     available_spaces.each do |as|
-      board[as.to_i] = @com
-      if game_is_over(board)
+      @board[as.to_i] = @com
+      return as.to_is if winner?
+    end
+    false
+  end
+
+  def block_oponent
+    available_spaces.each do |as|
+      @board[as.to_i] = @hum
+      return as.to_is if winner?
+    end
+    false
+  end
+
+  def random_move
+    available_spaces.sample.to_i
+  end
+
+
+
+
+  def get_best_move(next_player, depth = 0, best_score = {})
+    best_move = nil
+
+    available_spaces.each do |as|
+      @board[as.to_i] = @com
+      if winner?
         best_move = as.to_i
-        board[as.to_i] = as
+        @board[as.to_i] = as
         return best_move
-      else
-        board[as.to_i] = @hum
-        if game_is_over(board)
-          best_move = as.to_i
-          board[as.to_i] = as
-          return best_move
         else
-          board[as.to_i] = as
+        @board[as.to_i] = @hum
+        if winner?
+          best_move = as.to_i
+          @board[as.to_i] = as
+          return best_move
         end
       end
     end
+
     if best_move
       return best_move
     else
-      n = rand(0..available_spaces.count)
-      return available_spaces[n].to_i
+      random_move
     end
   end
 
-  def game_is_over(b)
-
-    [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
+  def winner?
+    [@board[0], @board[1], @board[2]].uniq.length == 1 ||
+    [@board[3], @board[4], @board[5]].uniq.length == 1 ||
+    [@board[6], @board[7], @board[8]].uniq.length == 1 ||
+    [@board[0], @board[3], @board[6]].uniq.length == 1 ||
+    [@board[1], @board[4], @board[7]].uniq.length == 1 ||
+    [@board[2], @board[5], @board[8]].uniq.length == 1 ||
+    [@board[0], @board[4], @board[8]].uniq.length == 1 ||
+    [@board[2], @board[4], @board[6]].uniq.length == 1
   end
 
-  def tie(b)
-    b.all? { |s| s == "X" || s == "O" }
+
+  def game_is_over(b=@board)
+    winner? || tie?
+  end
+
+  def tie?
+    !winner? && available_spaces.length == 0
+  end
+
+  def game_over?
+    winner? || tie?
   end
 
 end
